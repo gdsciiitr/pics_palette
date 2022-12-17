e936534bd39d0634151aa532ff534ebe921a6c2a
Gyanendra Tiwari <mrgyan432@gmail.com>
2022-12-17 14:43:18 +0530

    initial commit with 404 error page

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './NotFound.css'
+
+const NotFound = () => {
+  return (
+    <>
+      <h1>404</h1>
+      <div className="cloak__wrapper">
+        <div className="cloak__container">
+          <div className="cloak"></div>
+        </div>
+      </div>
+      <div className="info">
+        <h2>We can't find that page</h2>
+        <p>
+          We're fairly sure that page used to be here, but seems to have gone
+          missing. We do apologise on it's behalf.
+        </p>
+        <a href="/" target="_blank">
+          Home
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
