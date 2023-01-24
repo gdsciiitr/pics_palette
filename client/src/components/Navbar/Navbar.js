bce24b42354374e206e66259d6952c25835ad2d1
Gyanendra Tiwari <mrgyan432@gmail.com>
2023-01-24 13:16:05 +0530

    changing name

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -39,7 +39,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid col-md-10 col-12">
-        <NavLink className="navbar-brand fw-3 fs-2" to="/">Picture Book</NavLink>
+        <NavLink className="navbar-brand fw-3 fs-2" to="/">Pics Palette</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
